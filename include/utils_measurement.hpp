5163b2269c0677c020ccdd0c0b7bc7ee972020d8
kawnur <kawnur@yandex.ru>
2025-01-23 11:59:27 +0300

    Added utils_measurement

---
+++ include/utils_measurement.hpp
@@ -0,0 +1,34 @@
+#ifndef UTILS_MEASUREMENT_HPP
+#define UTILS_MEASUREMENT_HPP
+
+template<typename T, typename F, typename... Ts>
+T runMeasureDurationAndReturnResult(F&& func, Ts... params)
+{
+    auto begin = std::chrono::high_resolution_clock::now();
+
+    auto result = func(params...);
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+
+    auto elapsedCount = elapsed.count();
+    coutArgsWithSpaceSeparator("elapsed:", elapsedCount, "nanoseconds");
+
+    return result;
+}
+
+template<typename F, typename... Ts>
+void runAndMeasureDuration(F&& func, Ts... params)
+{
+    auto begin = std::chrono::high_resolution_clock::now();
+
+    func(params...);
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+
+    auto elapsedCount = elapsed.count();
+    coutArgsWithSpaceSeparator("elapsed:", elapsedCount, "nanoseconds");
+}
+
+#endif // UTILS_MEASUREMENT_HPP
