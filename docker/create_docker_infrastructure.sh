fffc675e600d36ec68cb78f09564d5ee73d05c3a
kawnur <kawnur@yandex.ru>
2026-09-18 17:34:24 +0300

    Updated docker files and added scripts

---
+++ docker/create_docker_infrastructure.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source ./.env
+
+# create volumes
+docker volume create chatapp_volume1
+docker volume create chatapp_volume2
+docker volume create chatapp_volume3
+
+# create network
+docker network create my-bridge-network --driver "bridge" --subnet $CHATAPP_BRIDGE_NETWORK_SUBNET
+
---
+++ docker/remove_docker_volumes.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# create volumes
+docker volume rm chatapp_volume1
+docker volume rm chatapp_volume2
+docker volume rm chatapp_volume3
